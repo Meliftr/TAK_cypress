3e6a47dc0892c98b0c109e30c3afaeeb18c5fb48
Melinda Fitriani <meli@Macbooks-MacBook-Air.local>
2023-05-29 14:34:29 +0700

    perubahan ke 2

---
+++ cypress/support/pageObject/tricentis/registerPage.js
@@ -0,0 +1,17 @@
+class registerPage {
+    registerMenu = '.ico-register'
+    genderFemale = '#gender-female'
+    firstName = '#FirstName'
+    lastName = '#LastName'
+    confirmPassword = '#ConfirmPassword'
+    registerBtn = '#register-button'
+    succsessMsg = '.result'
+    continueBtn = '.page-body > .buttons > .button-1'
+    welcomeMsg = '.topic-html-content-header'
+    errorEmailexist = '.validation-summary-errors > ul > li'
+    errorFirstname = ':nth-child(2) > .form-fields > :nth-child(2) > .field-validation-error > span'
+    errorEmail = ':nth-child(4) > .field-validation-error > span'
+    errorPassword = ':nth-child(1) > .field-validation-error > span'
+
+}
+export default registerPage;
